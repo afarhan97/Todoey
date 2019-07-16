15b096c0047a9cc5ad5fc37636ec64571d14ce7a
Ahmad Farhan <ahmad.farhan9715@live.com>
2019-07-16 14:45:25 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ahmad Farhan on 7/16/19.
+//  Copyright © 2019 Ahmad Farhan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
